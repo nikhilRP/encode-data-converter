887191640d6e9dac1565d1dc4ccce35187bb16f0
Nikhil Podduturi <nikhilrp@cherry-gpu1.stanford.edu>
2015-07-07 05:50:00 -0700

    Updated converter script

--- bin/Transform.py
+++ bin/Transform.py
@@ -1,4 +1,4 @@
-## https://www.encodeproject.org/search/?type=experiment&assay_term_name=ChIP-seq&assembly=hg19&target.investigated_as=transcription%20factor&files.file_type=bam&month_released=September,%202012
+# https://www.encodeproject.org/search/?type=experiment&month_released=December,%202014&assay_term_name=ChIP-seq&month_released=September,%202014&month_released=February,%202014&month_released=June,%202015&month_released=March,%202014&month_released=October,%202014&month_released=November,%202014&month_released=February,%202015&month_released=April,%202014
 
 import requests
 import boto
@@ -132,7 +132,7 @@ def main():
     )
     #parser.add_argument('--encode-url', help="ENCODE assay URL", required=True)
     args = parser.parse_args()
-    encode_url = 'https://www.encodeproject.org/search/?type=experiment&assay_term_name=ChIP-seq&assembly=hg19&target.investigated_as=transcription%20factor&files.file_type=bam&month_released=September,%202012'
+    encode_url = 'https://www.encodeproject.org/search/?type=experiment&month_released=December,%202014&assay_term_name=ChIP-seq&month_released=September,%202014&month_released=February,%202014&month_released=June,%202015&month_released=March,%202014&month_released=October,%202014&month_released=November,%202014&month_released=February,%202015&month_released=April,%202014'
     if encode_url:
         for exp in get_assay_JSON(encode_url):
             for f in exp.get('files', None):
